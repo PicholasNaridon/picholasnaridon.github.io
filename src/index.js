fcde56793bf7c3a21c357cabc0fd80068624ce61
picholasnaridon <paridonn1@gmail.com>
2018-05-23 21:18:29 -0400

    styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 import AppRouter from './Router/AppRouter';
-
+import './Homepage/assets/styles/reset.css'
+import './Homepage/assets/styles/static.css'
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
+registerServiceWorker();
